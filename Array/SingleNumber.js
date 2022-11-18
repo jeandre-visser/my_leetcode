ecbcb3b2876b058063e411d712fc2a3912a8c2da
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-18 12:54:27 -0700

    Add test cases

--- Array/SingleNumber.js
+++ Array/SingleNumber.js
@@ -6,3 +6,15 @@
   // USe XOR to get number that only occurs once
   return nums.reduce((a,b) => a^b)
 };
+
+// Test cases
+/**
+ * nums = [2,2,1]
+ * output 1
+ * 
+ * nums = [4,1,2,1,2]
+ * output 4
+ * 
+ * nums = [1]
+ * output 1
+ */
